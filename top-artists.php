b31a027c55910e9829d1ed5afca44ffdc615ab4b
Javi <github@snaven.com>
2014-06-26 22:05:02 +0200

    Last.fm
    Function to retrieve top albums of a user

---
+++ top-artists.php
@@ -0,0 +1,21 @@
+	<?php
+		$api_key = "xxx";
+		$username = "myemptywords";
+		$albums = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=$username&api_key=$api_key&format=json";
+
+		$json = file_get_contents($albums);
+		$data = json_decode($json, TRUE);
+
+		topArtist(10);
+
+		function topArtist($top){
+			global $data;
+			$id = 1;
+			foreach ($data['topartists']['artist'] as $band){
+	    		echo $id++.". Band: <img src='".$band['image'][3]['#text']."'/>". $band['name'] . ". Played ". $band['playcount'] ." times.<br>";
+				if (++$i == $top) break;
+			};
+		}
+
+	
+	?>
